234c6c6a831ae96278cb81c32f5c95c5f9868976
GHTaarn <62629455+GHTaarn@users.noreply.github.com>
2025-02-09 06:11:35 +0100

    Docstring for registrybrowser()

--- src/RegistryBrowser.jl
+++ src/RegistryBrowser.jl
@@ -11,6 +11,18 @@ modes = ["Package", "Versions", "Deps", "Compat", returnstr]
 
 pick_one(msg, options; kwargs...) = (println(msg); RadioMenu(options; kwargs...) |> request)
 
+"""
+    registrybrowser(packagepattern=""; registrypattern="")
+
+Launch an interactive browser in the terminal, making it possible to look
+through all registries that are available to the Julia session and get
+information about all packages in these registries.
+
+Calling this function with a `packagepattern` restricts the shown packages
+to packages whose names match `packagepattern`. Similarly, only registries
+whose names match `registrypattern` will be shown. `packagepattern` and
+`registrypattern` can be of either `AbstractString` or `Regex` type.
+"""
 function registrybrowser(packagepattern=""; registrypattern="")
     while true
         registries = filter(x->contains(x.name, registrypattern), Pkg.Registry.reachable_registries())
